84efb848c560221f3b021b6d23185ba4c6434fd8
Kevin Duff <kevinkelduff@gmail.com>
2018-06-30 13:44:24 +0100

    changed main a bit

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
